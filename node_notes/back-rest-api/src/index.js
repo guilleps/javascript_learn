5ae036640af87b5a0b4b1f25e1366be7b92cee37
Guillermo Palacios <nothicc04@gmail.com>
2024-09-08 23:24:21 -0500

    configurar la cadena de conexion de la  base de datos + instalamos mongoose

--- node_notes/back-rest-api/src/index.js
+++ node_notes/back-rest-api/src/index.js
@@ -3,6 +3,10 @@
 // sin la extension
 import app from "./app.js";
 
+import { connectDB } from "./db/db.js";
+
+connectDB();
+
 // funcion que permite al servidor escuchar en el puerto X de la maquina local
 // en etapa de desarrollo indicar el puerto es comun para hacer pruebas y demas
 // pero en produccion esto suele ser asignado como una variable constante o archivo de entorno dado el puerto asignado en el despliegue de la aplicacion
