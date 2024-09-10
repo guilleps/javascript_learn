23cd2eae4ea3c6d66c04558fafadbe4e72f1db23
Guillermo Palacios <nothicc04@gmail.com>
2024-09-10 11:20:45 -0500

    creacion de config + middleware + jwt

--- node_notes/back-rest-api/src/app.js
+++ node_notes/back-rest-api/src/app.js
@@ -5,8 +5,15 @@
 // como primer punto instalamos express (npm install express) e importamos express
 import express from "express";
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 // declarar variable app
 const app = express(); // igualamos a express pero siendo una funcion de manera que agregamos parentesis al final
 // de esta forma hacemos un instancia de express y luego lo exportamos
+
+app.use(express.json()); // middleware para convertir los req body o que el back lo pueda entender con express
+app.use(cors());
+app.use(cookieParser());
 
 export default app; // exportamos por defecto de la clase app.js al solo importarlo en otra clase como index.js
